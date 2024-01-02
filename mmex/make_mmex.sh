042aa8d7de5e48ff4fb293269a80fba7992a6b66
Pawel Zayakin <pz@magisters.net>
2024-01-02 01:23:35 +0000

    appstream

--- mmex/make_mmex.sh
+++ mmex/make_mmex.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y --no-install-recommends curl ca-certificates tcl build-essential ccache cmake file gettext git libcurl4-openssl-dev libwebkit2gtk-4.0-dev liblua5.3-dev lsb-release pkg-config rapidjson-dev
+apt-get install -y --no-install-recommends appstream curl ca-certificates tcl build-essential ccache cmake file gettext git libcurl4-openssl-dev libwebkit2gtk-4.0-dev liblua5.3-dev lsb-release pkg-config rapidjson-dev
 #wx3.0-i18n libwxgtk-webview3.0-gtk3-dev libgtk-3-dev
 cd /tmp
 
@@ -17,11 +17,11 @@ popd
 
 MMEX="1.7.0"
 git clone --recursive https://github.com/moneymanagerex/moneymanagerex.git
+set advice.detachedHead=false
 pushd moneymanagerex && git checkout v$MMEX && popd
 mkdir moneymanagerex/build
 pushd moneymanagerex/build
 export MAKEFLAGS=-j8
-
 cmake -DCMAKE_CXX_FLAGS="-w" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxWidgets-$WX/build_gtk/wx-config -DwxWidgets_LIBRARIES=/tmp/wxWidgets-$WX/build_gtk/lib -DwxWidgets_INCLUDE_DIRS=/tmp/wxWidgets-$WX/include -Wno-dev .. # -DCMAKE_PREFIX_PATH==/tmp/wxWidgets/build_gtk
 sed -i 's!Debian.n/a!Debian!g' CPackConfig.cmake
 cp /tmp/moneymanagerex/resources/dist/linux/share/metainfo/org.moneymanagerex.MMEX.metainfo.xml.in /tmp/moneymanagerex/org.moneymanagerex.MMEX.metainfo.xml
@@ -29,5 +29,5 @@ cmake --build . --target package
 mv ./mmex*.deb /  # sudo apt install /mmex*.deb && rm ~/Downloads/mmex*.deb
 popd
 rm -fr moneymanagerex wxWidgets-$WX
-apt-get purge -y tcl build-essential ccache cmake file gettext git libcurl4-openssl-dev libwebkit2gtk-4.0-dev liblua5.3-dev lsb-release pkg-config rapidjson-dev && \
+apt-get purge -y tcl appstream build-essential ccache cmake file gettext git libcurl4-openssl-dev libwebkit2gtk-4.0-dev liblua5.3-dev lsb-release pkg-config rapidjson-dev && \
 apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/*
