34e53977240f502b71618dddf9901ff1182b0c80
Pawel Zayakin <38588587+zajakin@users.noreply.github.com>
2024-05-01 00:57:14 +0300

    Update make_mmex.sh

--- mmex/make_mmex.sh
+++ mmex/make_mmex.sh
@@ -15,7 +15,7 @@ pushd wxWidgets-$WX/build_gtk
 make
 popd
 
-MMEX="1.7.0"
+MMEX="1.8.0"
 git clone --recursive https://github.com/moneymanagerex/moneymanagerex.git
 set advice.detachedHead=false
 pushd moneymanagerex && git checkout v$MMEX && popd
