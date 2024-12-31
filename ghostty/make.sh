581eaefc38606ccc5277766bcb7e5ae88b66d395
Pawel Zayakin <pz@magisters.net>
2024-12-31 01:01:32 +0000

    ghostty

--- ghostty/make.sh
+++ ghostty/make.sh
@@ -25,8 +25,8 @@ Architecture: all
 Description: Ghostty terminal emulator
 END
 
- dpkg-deb --build ghostty ghostty_$VER_amd64.deb
+ dpkg-deb --build -v ghostty ghostty_${VER}_amd64.deb
 #  dpkg -i ghostty.deb
  rm -rf /tmp/* /opt/* /ghostty/*
- apt-get purge -y curl ca-certificates libgtk-4-dev libadwaita-1-dev git pkg-config xz-utilscurl ca-certificates libgtk-4-dev libadwaita-1-dev git pkg-config xz-utils && \
+ apt-get purge -y curl ca-certificates libgtk-4-dev libadwaita-1-dev git pkg-config xz-utils && \
  apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/*
